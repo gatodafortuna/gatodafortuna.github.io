34818074c1dfd48aa29c7fa7c714e968abdb8449
Teste <44550963+gatodafortuna@users.noreply.github.com>
2020-01-30 19:09:46 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,122 +1,61 @@
-var gems_temp = 0
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Gato da Fortuna</title>
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+<script src="main.js"></script>
+<link rel="stylesheet" type="text/css" href="style.css">
+<!-- Global site tag (gtag.js) - Google Analytics -->
 
-$(document).on("click","#btn",function(){
-	var gems = $("input[name=gems]").val();
-	gems_temp = gems;
-	if (gems >= 20) {
-		var min=21;
-		var max=28;
-		var random = Math.floor(min + Math.random()*(max + 1 - min));
-		var total = Number(gems) + Math.abs((Number(random) - (Number(min) - 1)))
-		$("input[name=gems]").val(total);
-		$("#20").text("1 : Roll 20 a 28 = " + random);
-		var ganho = Number(total) - Number(gems_temp)
-		$("#ganho").text("Gemas recebidas = " + ganho);
-		$("#btn").attr("id", "btn2");
-	}
-});
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-157377928-1"></script>
 
-$(document).on("click","#btn2",function(){
-	var gems = $("input[name=gems]").val();
-	if (gems >= 30) {
-		var min=31;
-		var max=48;
-		var random = Math.floor(min + Math.random()*(max + 1 - min));
-		var total = Number(gems) + Math.abs((Number(random) - (Number(min) - 1)))
-		$("input[name=gems]").val(total);
-		$("#30").text("2 : Roll 30 a 48 = " + random);
-		var ganho = Number(total) - Number(gems_temp)
-		$("#ganho").text("Gemas recebidas = " + ganho);
-		$("#btn2").attr("id", "btn3");
-	};
-});
+<script>
 
-$(document).on("click","#btn3",function(){
-	var gems = $("input[name=gems]").val();
-	if (gems >= 65) {
-		var min=66;
-		var max=88;
-		var random = Math.floor(min + Math.random()*(max + 1 - min));
-		var total = Number(gems) + Math.abs((Number(random) - (Number(min) - 1)))
-		$("input[name=gems]").val(total);
-		$("#65").text("3 : Roll 65 a 88 = " + random)
-		var ganho = Number(total) - Number(gems_temp)
-		$("#ganho").text("Gemas recebidas = " + ganho);
-		$("#btn3").attr("id", "btn4");
-	};
-});
+  window.dataLayer = window.dataLayer || [];
 
-$(document).on("click","#btn4",function(){
-	var gems = $("input[name=gems]").val();
-	if (gems >= 150) {
-		var min=151;
-		var max=188;
-		var random = Math.floor(min + Math.random()*(max + 1 - min));
-		var total = Number(gems) + Math.abs((Number(random) - (Number(min) - 1)))
-		$("input[name=gems]").val(total);
-		$("#150").text("4 : Roll 150 a 188 = " + random);
-		var ganho = Number(total) - Number(gems_temp)
-		$("#ganho").text("Gemas recebidas = " + ganho);
-		$("#btn4").attr("id", "btn5");
-	};
-});
+  function gtag(){dataLayer.push(arguments);}
 
-$(document).on("click","#btn5",function(){
-	var gems = $("input[name=gems]").val();
-	if (gems >= 240) {
-		var min=241;
-		var max=288;
-		var random = Math.floor(min + Math.random()*(max + 1 - min));
-		var total = Number(gems) + Math.abs((Number(random) - (Number(min) - 1)))
-		$("input[name=gems]").val(total);
-		$("#240").text("5 : Roll 240 a 288 = " + random);
-		var ganho = Number(total) - Number(gems_temp)
-		$("#ganho").text("Gemas recebidas = " + ganho);
-		$("#btn5").attr("id", "btn6");
-	};
-});
+  gtag('js', new Date());
 
-$(document).on("click","#btn6",function(){
-	var gems = $("input[name=gems]").val();
-	if (gems >= 330) {
-		var min=331;
-		var max=388;
-		var random = Math.floor(min + Math.random()*(max + 1 - min));
-		var total = Number(gems) + Math.abs((Number(random) - (Number(min) - 1)))
-		$("input[name=gems]").val(total);
-		$("#330").text("6 : Roll 330 a 388 = " + random);
-		var ganho = Number(total) - Number(gems_temp)
-		$("#ganho").text("Gemas recebidas = " + ganho);
-		$("#btn6").attr("id", "btn7");
-	};
-});
+ 
 
-$(document).on("click","#btn7",function(){
-	var gems = $("input[name=gems]").val();
-	if (gems >= 500) {
-		var min=501;
-		var max=588;
-		var random = Math.floor(min + Math.random()*(max + 1 - min));
-		var total = Number(gems) + Math.abs((Number(random) - (Number(min) - 1)))
-		$("input[name=gems]").val(total);
-		$("#500").text("7 : Roll 500 a 588 = " + random);
-		var ganho = Number(total) - Number(gems_temp)
-		$("#ganho").text("Gemas recebidas = " + ganho);
-		$("#btn7").attr("id", "btn8");
-	};
-});
+  gtag('config', 'UA-157377928-1');
 
-$(document).on("click","#btn8",function(){
-	var gems = $("input[name=gems]").val();
-	if (gems >= 660) {
-		var min=661;
-		var max=888;
-		var random = Math.floor(min + Math.random()*(max + 1 - min));
-		var total = Number(gems) + Math.abs((Number(random) - (Number(min) - 1)))
-		$("input[name=gems]").val(total);
-		$("#660").text("8 : Roll 660 a 888 = " + random);
-		var ganho = Number(total) - Number(gems_temp)
-		$("#ganho").text("Gemas recebidas = " + ganho);
-		$("#btn8").attr("id", "btn9");
-	};
-});
+</script>
+</head>
+
+<body style="color: cyan; font-family: arial; margin: auto; width: 80%;">
+<div style="background-color: rgba(0, 0, 0, 0.90); padding: 10px">
+<h1 style="text-align: center;">Simulador Gato da Fortuna</h1>
+<h4 style="text-align: center;">Se preparem para o gato toda 1ª segunda-feira de cada mês</h4>
+<br>
+<h3>Venham fazer parte da nossa legião LegiãoUrbana no A18</h3>
+<br>
+<br>
+<div style="display: flex;">
+<input type="text" name="gems"><br>
+<button id="btn" style="margin-left: 10px; padding: 4px">Começar</button>
+</div>
+<br>
+<p id=20>1 : Roll 20 a 28 =</p>
+<p id=30>2 : Roll 30 a 48 =</p>
+<p id=65>3 : Roll 65 a 88 =</p>
+<p id=150>4 : Roll 150 a 188 =</p>
+<p id=240>5 : Roll 240 a 288 =</p>
+<p id=330>6 : Roll 330 a 388 =</p>
+<p id=500>7 : Roll 500 a 588 =</p>
+<p id=660>8 : Roll 660 a 888 =</p>
+<p id=ganho>Gemas recebidas =</p>
+<br>
+<p>Dica : Junte 500 gemas, caso você tenha sorte de conseguir realizar os 8 rolls do gato, você será capaz de dar 10 tiros por dia para sempre.
+<br>
+(300 tiros por mês, 10 tiros por dia, 90 gemas de restituição por mês, 500 gemas para o próx. gato)</p>
+<br>
+<footer style="height: 200px;">by Filhão / (Hagenz) / Zeu / Bigodera / Severa</footer>
+</div>
+</body>
+</html>
